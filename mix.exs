5cf9b7565daa177378443f19a578f2dcc8e3b65b
Matt Miller <mwm@fastmail.com>
2019-01-13 08:56:12 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Ed25519.Mixfile do
   def project do
     [
       app: :ed25519,
-      version: "1.3.0",
-      elixir: "~> 1.5",
+      version: "1.3.1",
+      elixir: "~> 1.7",
       name: "Ed25519",
       source_url: "https://github.com/mwmiller/ed25519_ex",
       build_embedded: Mix.env() == :prod,
@@ -24,7 +24,7 @@ defmodule Ed25519.Mixfile do
     [
       {:earmark, "~> 1.0", only: :dev},
       {:ex_doc, "~> 0.18", only: :dev},
-      {:credo, "~> 0.8", only: [:dev, :test]}
+      {:credo, "~> 1.0", only: [:dev, :test]}
     ]
   end
 
