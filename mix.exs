4619c42e7caab1602b00b1aa80bac542bf44f0b6
Matthew Miller <matt.miller@nfiindustries.com>
2021-10-25 15:47:47 -0400

    Bump version and update deps.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ed25519.Mixfile do
   def project do
     [
       app: :ed25519,
-      version: "1.3.2",
+      version: "1.3.3",
       elixir: "~> 1.7",
       name: "Ed25519",
       source_url: "https://github.com/mwmiller/ed25519_ex",
